a56f5c0ea9f157257218b993116d408de0d04353
masud-rana44 <mdmasudrana4488@gmail.com>
2023-07-28 21:40:23 +0600

    header main and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { faker } from "@faker-js/faker";
 import { useState } from "react";
 import "./styles.css";
 
-function createRandomPosts() {
+function createRandomPost() {
   return {
     title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
     body: faker.hacker.phrase(),
@@ -11,12 +11,16 @@ function createRandomPosts() {
 
 function App() {
   const [posts, setPosts] = useState(
-    Array.from({ length: 30 }, () => createRandomPosts())
+    Array.from({ length: 30 }, () => createRandomPost())
   );
   const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
 
-  const searchedPosts = () => {};
+  const searchedPosts = posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
 
   function handleClearPosts() {
     setPosts([]);
@@ -30,11 +34,12 @@ function App() {
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
       />
+      <Main posts={searchedPosts} onAddPost={handleAddPost} />
+      <Archive onAddPost={handleAddPost} />
+      <Footer />
     </section>
   );
 }
-
-export default App;
 
 function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
   return (
@@ -62,3 +67,97 @@ function SearchPosts({ searchQuery, setSearchQuery }) {
     />
   );
 }
+
+function Main({ posts, onAddPost }) {
+  return (
+    <main>
+      <FormAddPost onAddPost={onAddPost} />
+      <Posts posts={posts} />
+    </main>
+  );
+}
+
+function Posts({ posts }) {
+  return (
+    <section>
+      <List posts={posts} />
+    </section>
+  );
+}
+
+function List({ posts }) {
+  return (
+    <ul>
+      {posts.map((post, i) => (
+        <li key={i}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function FormAddPost({ onAddPost }) {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    if (!body || !title) return;
+    onAddPost({ title, body });
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Post title"
+      />
+      <textarea
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        placeholder="Post body"
+      />
+      <button>Add post</button>
+    </form>
+  );
+}
+
+function Footer() {
+  return <footer>&copy; by The Atomic Blog ✌️</footer>;
+}
+
+function Archive({ onAddPost }) {
+  const [posts] = useState(() =>
+    Array.from({ length: 100 }, () => createRandomPost())
+  );
+  const [showArchive, setShowArchive] = useState(false);
+
+  return (
+    <aside>
+      <h2>Post archive</h2>
+      <button onClick={() => setShowArchive((s) => !s)}>
+        {showArchive ? "Hide archive posts" : "Show archive posts"}
+      </button>
+
+      {showArchive && (
+        <ul>
+          {posts.map((post, i) => (
+            <li key={i}>
+              <p>
+                <strong>{post.title}:</strong> {post.body}
+              </p>
+              <button onClick={() => onAddPost(post)}>Add as new post</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </aside>
+  );
+}
+
+export default App;
